a5b724e054197394fd85e53bb3d4971045f5b178
DevTake44 <tfunabiki@taiko.net>
2026-08-18 17:20:56 +0900

    Delete page (13).tsx

--- page (13).tsx
+++
@@ -1,9 +0,0 @@
-import LifeCheck from "@/components/LifeCheck";
-
-// このページはブラウザ内だけで完結する(サーバー/DBを使わない)ため、
-// 他ページのようなSupabaseからのデータ取得は不要。
-export const dynamic = "force-dynamic";
-
-export default function LifeCheckPage() {
-  return <LifeCheck />;
-}
