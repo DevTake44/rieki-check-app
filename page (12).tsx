9edc801b1dc29f04738a612d6ab344349efcfc98
DevTake44 <tfunabiki@taiko.net>
2026-08-18 17:21:05 +0900

    Delete page (12).tsx

--- page (12).tsx
+++
@@ -1,9 +0,0 @@
-import PayableCheck from "@/components/PayableCheck";
-
-// このページはブラウザ内だけで完結する(サーバー/DBを使わない)ため、
-// 他ページのようなSupabaseからのデータ取得は不要。
-export const dynamic = "force-dynamic";
-
-export default function PayableCheckPage() {
-  return <PayableCheck />;
-}
