1bf25753c4419e73d8a4807bd16a672b26660d8e
DevTake44 <tfunabiki@taiko.net>
2026-08-18 17:21:14 +0900

    Delete page (11).tsx

--- page (11).tsx
+++
@@ -1,9 +0,0 @@
-import LifeBillingCheck from "@/components/LifeBillingCheck";
-
-// このページはブラウザ内だけで完結する(サーバー/DBを使わない)ため、
-// 他ページのようなSupabaseからのデータ取得は不要。
-export const dynamic = "force-dynamic";
-
-export default function LifeBillingCheckPage() {
-  return <LifeBillingCheck />;
-}
