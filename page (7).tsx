7845f6f1296bf1be0027edc92818bd73cf1d71bb
DevTake44 <tfunabiki@taiko.net>
2026-08-18 17:20:14 +0900

    Delete page (7).tsx

--- page (7).tsx
+++
@@ -1,9 +0,0 @@
-import BenrinetCheck from "@/components/BenrinetCheck";
-
-// このページはブラウザ内だけで完結する(サーバー/DBを使わない)ため、
-// 他ページのようなSupabaseからのデータ取得は不要。
-export const dynamic = "force-dynamic";
-
-export default function BenrinetCheckPage() {
-  return <BenrinetCheck />;
-}
